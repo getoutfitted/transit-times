da420f0a256a208efb2a46fd36edc44bf550b798
Paul Grever <paulgrever@gmail.com>
2016-08-01 15:19:31 -0600

    added less file to dashboard

--- server/api/transitTimes.js
+++ server/api/transitTimes.js
@@ -1,4 +1,4 @@
-import _ from 'underscore';
+import { _ } from 'meteor/underscore';
 import moment from 'moment';
 import { Meteor } from 'meteor/meteor';
 import { TransitTimesCache } from '../../lib/collections';
