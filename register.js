82a712d568cc655fe46209884349884bfa9ba8ae
Paul Grever <paulgrever@gmail.com>
2016-08-19 14:16:43 -0600

    added transit times layout for custom go

--- register.js
+++ register.js
@@ -1,4 +1,5 @@
-ReactionCore.registerPackage({
+import { Reaction } from '/server/api';
+Reaction.registerPackage({
   label: 'Transit Time Calculator',
   name: 'transit-times',
   icon: 'fa fa-calculator',
@@ -13,7 +14,7 @@ ReactionCore.registerPackage({
     container: 'getoutfitted',
     icon: 'fa fa-calculator',
     template: 'transitTimesDashboard',
-    workflow: 'coreWorkflow',
+    workflow: 'transtTimesWorkflow',
     priority: 3
   }, {
     route: '/dashboard/transit-times/settings',
@@ -21,5 +22,32 @@ ReactionCore.registerPackage({
     label: 'GetOutfitted Transit Time Settings',
     name: 'transitTimesSettings',
     template: 'transitTimesSettings'
+  }],
+  layout: [{
+    workflow: 'transitTimesWorkflow',
+    layout: 'coreLayout',
+    theme: 'default',
+    enabled: true,
+    structure: {
+      template: 'transitTimesDashboard',
+      layoutHeader: 'goLayoutHeader',
+      layoutFooter: 'goLayoutFooter',
+      notFound: 'goNotFound',
+      dashboardControls: 'dashboardControls',
+      adminControlsFooter: 'adminControlsFooter'
+    }
+  }, {
+    workflow: 'transitTimesWorkflow',
+    layout: 'getoutfittedLayout',
+    theme: 'default',
+    enabled: true,
+    structure: {
+      template: 'transitTimesDashboard',
+      layoutHeader: 'goLayoutHeader',
+      layoutFooter: 'goLayoutFooter',
+      notFound: 'goNotFound',
+      dashboardControls: 'dashboardControls',
+      adminControlsFooter: 'adminControlsFooter'
+    }
   }]
 });
